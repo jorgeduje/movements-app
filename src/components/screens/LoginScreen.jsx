54ac31e021326902106fe7c5da1569b839ea8d49
jorgeduje <jorgeduje4@gmail.com>
2023-10-20 11:44:59 -0300

    primeros archivos

---
+++ src/components/screens/LoginScreen.jsx
@@ -0,0 +1,74 @@
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Input } from "@rneui/themed";
+import { Button } from "@rneui/base";
+
+export default function LoginScreen({ navigation }) {
+  const { top } = useSafeAreaInsets();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = () => {
+    console.log(user);
+    navigation.navigate("HomeScreen");
+  };
+
+  return (
+    <SafeAreaView style={{ top: top, ...styles.containerLogin }}>
+      <View style={{ marginHorizontal: 20 }}>
+        <Text style={styles.title}>Iniciar sesion</Text>
+
+        <View>
+          <Input
+            placeholder="Email"
+            placeholderTextColor={"white"}
+            inputStyle={{ color: "white" }}
+            onChangeText={(text) => setUser({ ...user, email: text })}
+          />
+          <Input
+            placeholder="Contraseña"
+            placeholderTextColor={"white"}
+            inputStyle={{ color: "white" }}
+            secureTextEntry={true}
+            onChangeText={(text) => setUser({ ...user, password: text })}
+          />
+        </View>
+
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Button
+            title={"Ingresar"}
+            type="outline"
+            containerStyle={{
+              backgroundColor: "#00ffa8",
+              width: 200,
+            }}
+            buttonStyle={{
+              borderColor: "#00ffa8",
+              border: 1,
+            }}
+            titleStyle={{ color: "black" }}
+            onPress={handleSubmit}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerLogin: {
+    backgroundColor: "#161717",
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    color: "whitesmoke",
+    fontSize: 30,
+    textAlign: "center",
+    marginBottom: 50,
+  },
+});
---
+++ src/router/NavigationStack.jsx
@@ -0,0 +1,18 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../components/screens/LoginScreen";
+import HomeScreen from "../components/screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+export default function NavigationStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
